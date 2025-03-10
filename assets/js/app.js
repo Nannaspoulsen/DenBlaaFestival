30ade3c6c794a6a853488080bcb60e8bae914c71
Nannaspoulsen <Nannaspoulsen@hotmail.com>
2025-03-10 14:08:54 +0100

    Navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,77 +3,92 @@ document.addEventListener("DOMContentLoaded", () => {
   {
     spillested: "John Bull Pub",
     artist: "TogoRhino - African Blues",
-    spilletid: "Fredag, kl. 23:00"
+    spilletid: "Fredag, kl. 23:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "John Bull Pub",
     artist: "Johnny Burgin Trio",
-    spilletid: "Lørdag, kl. 23:00"
+    spilletid: "Lørdag, kl. 23:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "Salling Rooftop",
     artist: "HP Lange & Jarno Varsted",
-    spilletid: "Fredag, kl. 17:00"
+    spilletid: "Fredag, kl. 17:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "Salling Rooftop",
     artist: "Kwella/Sejthen Duo",
-    spilletid: "Søndag, kl. 14:00"
+    spilletid: "Søndag, kl. 14:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "Salling Rooftop",
     artist: "Sahra da Silva & Alain Apaloo",
-    spilletid: "Lørdag, kl. 14:00"
+    spilletid: "Lørdag, kl. 14:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "Kulturcenter Fjordbyen",
     artist: "Big Creek Slim & Johnny Burgin",
-    spilletid: "Fredag, kl. 15:00"
+    spilletid: "Fredag, kl. 15:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "Kulturcenter Fjordbyen",
     artist: "Jesper Theis & RJ Mischo",
-    spilletid: "Lørdag, kl. 15:00"
+    spilletid: "Lørdag, kl. 15:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "Duus Vinkælder",
     artist: "Wash & Kjær",
-    spilletid: "Fredag, kl. 21:00"
+    spilletid: "Fredag, kl. 21:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "Duus Vinkælder",
     artist: "Villy Moesgaard Jazz4",
-    spilletid: "Lørdag, kl. 21:00"
+    spilletid: "Lørdag, kl. 21:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "Kaffefair",
     artist: "The Downshifters",
-    spilletid: "Fredag, kl. 15:00"
+    spilletid: "Fredag, kl. 15:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "Kaffefair",
     artist: "Late Lunch and Laidback Grooves",
-    spilletid: "Lørdag, kl. 15:00"
+    spilletid: "Lørdag, kl. 15:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "Kystens Perle",
     artist: "Malthe Nielsen & Henning Jensen",
-    spilletid: "Fredag, kl. 15:00"
+    spilletid: "Fredag, kl. 15:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "Kystens Perle",
     artist: "The Muleskinners",
-    spilletid: "Lørdag, kl. 15:00"
+    spilletid: "Lørdag, kl. 15:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "El Mariachi",
     artist: "Jimi and Double af Trouble",
-    spilletid: "Fredag, kl. 23:00"
+    spilletid: "Fredag, kl. 23:00",
+    beskrivelse: "Lorem Ipsum.........."
   },
   {
     spillested: "El Mariachi",
     artist: "Sweet Marta og Johnny BigStone",
-    spilletid: "Lørdag, kl. 22:00"
+    spilletid: "Lørdag, kl. 22:00",
+    beskrivelse: "Lorem Ipsum.........."
   }
 ];
 //Vi kan også indsætte billede af hver artist i denne liste. Vi skal bare blive enige om, hvilket billede. 
