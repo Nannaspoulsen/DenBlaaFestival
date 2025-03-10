19b38cdc4e72c03ac3e60b789f0645e0515bf55a
Nannaspoulsen <Nannaspoulsen@hotmail.com>
2025-03-10 13:02:07 +0100

    Tilføjet alle artister i et array af objekter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,78 @@
+const events = [
+  {
+    spillested: "John Bull Pub",
+    artist: "TogoRhino - African Blues",
+    spilletid: "Fredag, kl. 23:00"
+  },
+  {
+    spillested: "John Bull Pub",
+    artist: "Johnny Burgin Trio",
+    spilletid: "Lørdag, kl. 23:00"
+  },
+  {
+    spillested: "Salling Rooftop",
+    artist: "HP Lange & Jarno Varsted",
+    spilletid: "Fredag, kl. 17:00"
+  },
+  {
+    spillested: "Salling Rooftop",
+    artist: "Kwella/Sejthen Duo",
+    spilletid: "Søndag, kl. 14:00"
+  },
+  {
+    spillested: "Salling Rooftop",
+    artist: "Sahra da Silva & Alain Apaloo",
+    spilletid: "Lørdag, kl. 14:00"
+  },
+  {
+    spillested: "Kulturcenter Fjordbyen",
+    artist: "Big Creek Slim & Johnny Burgin",
+    spilletid: "Fredag, kl. 15:00"
+  },
+  {
+    spillested: "Kulturcenter Fjordbyen",
+    artist: "Jesper Theis & RJ Mischo",
+    spilletid: "Lørdag, kl. 15:00"
+  },
+  {
+    spillested: "Duus Vinkælder",
+    artist: "Wash & Kjær",
+    spilletid: "Fredag, kl. 21:00"
+  },
+  {
+    spillested: "Duus Vinkælder",
+    artist: "Villy Moesgaard Jazz4",
+    spilletid: "Lørdag, kl. 21:00"
+  },
+  {
+    spillested: "Kaffefair",
+    artist: "The Downshifters",
+    spilletid: "Fredag, kl. 15:00"
+  },
+  {
+    spillested: "Kaffefair",
+    artist: "Late Lunch and Laidback Grooves",
+    spilletid: "Lørdag, kl. 15:00"
+  },
+  {
+    spillested: "Kystens Perle",
+    artist: "Malthe Nielsen & Henning Jensen",
+    spilletid: "Fredag, kl. 15:00"
+  },
+  {
+    spillested: "Kystens Perle",
+    artist: "The Muleskinners",
+    spilletid: "Lørdag, kl. 15:00"
+  },
+  {
+    spillested: "El Mariachi",
+    artist: "Jimi and Double af Trouble",
+    spilletid: "Fredag, kl. 23:00"
+  },
+  {
+    spillested: "El Mariachi",
+    artist: "Sweet Marta og Johnny BigStone",
+    spilletid: "Lørdag, kl. 22:00"
+  }
+];
 
